86590f068c84ac1daad54c029bd18e462da8cdcc
PoolloverNathan <poollovernathan@noreply.codeberg.org>
2025-11-13 00:19:05 -0500

    UUID fixing

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,8 @@ val erd by tasks.register<Sync>("myExtractRenderDoc") {
     into("$buildDir/renderdoc")
 }
 
+loom.runs["client"].programArgs += listOf("--username", "Player", "--uuid", "bd346dd5-ac1c-427d-87e8-73bdd4bf3e13")
+
 tasks.named("runClientRenderDoc") {
     dependsOn(erd)
 }
