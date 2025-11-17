b5c6e32907deb96babc77d41430158d46b1201bb
PoolloverNathan <poollovernathan@noreply.codeberg.org>
2025-11-16 22:13:59 -0500

    FOR OLIVE!!!

--- src/main/scala/org/eu/net/pool/hexic/Hexic.scala
+++ src/main/scala/org/eu/net/pool/hexic/Hexic.scala
@@ -1804,8 +1804,14 @@ def getPocketID(key: Identifier): Option[UUID] =
     Some(UUID(bi1.longValue, bi2.longValue))
   else
     None
-val pocketNames: UUID => String = memo {
-  _.toString // TODO
+def pocketName(using rand: java.util.Random) =
+  def b = "zxcvbnm"(rand.nextInt(7)).toString
+  def v = "aeiouaeiouaeiouy"(rand.nextInt(16)).toString
+  def piece = s"${b.toUpperCase}$v$b" + Iterator.continually(v + b).takeWhile(_ => rand.nextInt(3) != 0).mkString("")
+  (piece +: Iterator.continually(piece).takeWhile(_ => rand.nextInt(5) == 0).toSeq).mkString("-")
+val pocketNames: UUID => String = memo { id =>
+  given Random = Random(id.getLeastSignificantBits)
+  pocketName
 }
 
 abstract case class AbstractMetatableIota(iotaType: MetatableIotaType & Singleton, userdata: Iota, override val display: Text, metatable: String, readonlyMetatable: Boolean) extends Iota(iotaType, (userdata, display, metatable, readonlyMetatable)):
