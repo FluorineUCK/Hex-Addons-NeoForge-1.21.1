38549146e84be8dde25882a02c19787b0afdd18b
PoolloverNathan <poollovernathan@noreply.codeberg.org>
2025-12-01 17:07:05 +0100

    allow using mavenLocal for plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,11 @@ pluginManagement {
         maven("https://maven.fabricmc.net/") {
             name = "Fabric"
         }
+        mavenLocal() {
+            content {
+                includeGroupAndSubgroups("org.eu.net.pool")
+            }
+        }
         maven("https://maven.pool.net.eu.org/") {
             content {
                 includeGroupAndSubgroups("org.eu.net.pool")
