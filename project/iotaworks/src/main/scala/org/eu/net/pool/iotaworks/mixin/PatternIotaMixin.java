4bb9630f9f55e5921d06bdbd77df973399a4952c
poolcritter <pool@pool.net.eu.org>
2025-12-16 09:29:36 -0500

    pattern level rendering

---
+++ project/iotaworks/src/main/scala/org/eu/net/pool/iotaworks/mixin/PatternIotaMixin.java
@@ -0,0 +1,40 @@
+package org.eu.net.pool.iotaworks.mixin;
+
+import at.petrak.hexcasting.api.casting.iota.PatternIota;
+import at.petrak.hexcasting.api.casting.math.HexPattern;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.minecraft.text.Text;
+import net.minecraft.text.MutableText;
+import scala.collection.mutable.StringBuilder;
+
+import org.eu.net.pool.iotaworks.HexPatternAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PatternIota.class)
+public class PatternIotaMixin {
+    @WrapMethod(method = "display(Lat/petrak/hexcasting/api/casting/math/HexPattern;)V")
+    private static Text wrappedDisplay(HexPattern pat, Operation<Text> original) {
+        int level = ((HexPatternAccessor) (Object) pat).depth();
+        StringBuilder buf = new StringBuilder();
+        while (level > 0) {
+            switch (level % 10) {
+                case 0: buf.addOne('⁰'); break;
+                case 1: buf.addOne('¹'); break;
+                case 2: buf.addOne('²'); break;
+                case 3: buf.addOne('³'); break;
+                case 4: buf.addOne('⁴'); break;
+                case 5: buf.addOne('⁵'); break;
+                case 6: buf.addOne('⁶'); break;
+                case 7: buf.addOne('⁷'); break;
+                case 8: buf.addOne('⁸'); break;
+                case 9: buf.addOne('⁹'); break;
+            }
+            level /= 10;
+        }
+        buf.reverse();
+        return ((MutableText) original.call(pat)).append(buf.toString());
+    }
+}
