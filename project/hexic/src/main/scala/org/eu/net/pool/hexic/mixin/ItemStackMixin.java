812fb6a5ce09382ad65ab76ed7d2ef92e3a8a7f0
poolcritter <pool@pool.net.eu.org>
2026-04-12 18:02:11 -0400

    disable negative item stacks by default

--- project/hexic/src/main/scala/org/eu/net/pool/hexic/mixin/ItemStackMixin.java
+++ project/hexic/src/main/scala/org/eu/net/pool/hexic/mixin/ItemStackMixin.java
@@ -5,11 +5,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
+import org.eu.net.pool.hexic.cfg;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import scala.util.CommandLineParser;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
@@ -20,6 +22,10 @@ public class ItemStackMixin {
         if (nbt.contains("Count", NbtElement.INT_TYPE)) {
             count = nbt.getInt("Count");
         }
+        int minSize = cfg.apply("hexic.min_stack_size", Integer::parseInt).getOrElse(() -> 0);
+        int maxSize = cfg.apply("hexic.max_stack_size", Integer::parseInt).getOrElse(() -> Integer.MAX_VALUE);
+        if (count < minSize) count = minSize;
+        if (count > maxSize) count = maxSize;
     }
 
     @WrapOperation(method = {"writeNbt", "method_7953"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtCompound;putByte(Ljava/lang/String;B)V"))
